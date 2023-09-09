c9bc09ebee0c1440f8d927b1102775f9be65e3ea
Morpheus <mangoo.pay@gmail.com>
2023-09-09 18:52:15 -0400

    add persisgate commit

--- src/store.js
+++ src/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/Counter/counterSlice'
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer
-  }
-})
+import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, counterReducer)
+
+export default () => {
+  let store = createStore(persistedReducer)
+  let persistor = persistStore(store)
+  return { store, persistor }
+}
